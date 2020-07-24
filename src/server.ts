1d0f6f44b8bba9a0a0e3e05c6a5ae10bfdd3a390
eguchi <eguchi@asial.co.jp>
2020-07-24 09:37:43 +0900

    Object scheme type

--- src/server.ts
+++ src/server.ts
@@ -19,20 +19,42 @@ app.use((req, res, next) => {
 
 /* スキーマ */
 const schema = buildSchema(`
+  type RandomDie {
+    numSides: Int!
+    rollOnce: Int!
+    roll(numRolls: Int!): [Int]
+  }
+
   type Query {
-    rollDice(numDice: Int!, numSides: Int): [Int]
+    getDie(numSides: Int): RandomDie
   }
 `);
-/* リゾルバ */
-const rootResolver = {
-  rollDice: ({ numDice, numSides }: { numDice: number, numSides: number }) => {
-    var output = [];
-    for (var i = 0; i < numDice; i++) {
-      output.push(1 + Math.floor(Math.random() * (numSides || 6)));
+
+class RandomDie {
+  numSides: number;
+
+  constructor(numSides: number) {
+    this.numSides = numSides;
+  }
+
+  rollOnce() {
+    return 1 + Math.floor(Math.random() * this.numSides);
+  }
+
+  roll({ numRolls }: { numRolls: number }) {
+    const output = [];
+    for (let i = 0; i < numRolls; i++) {
+      output.push(this.rollOnce());
     }
     return output;
   }
-};
+}
+
+const rootResolver = {
+  getDie: ({ numSides }: { numSides: number }) => {
+    return new RandomDie(numSides || 6);
+  }
+}
 
 app.use('/', graphqlHTTP({
   schema: schema,
